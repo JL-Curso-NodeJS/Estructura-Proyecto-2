29d22e5dfd4fb07717c352029b75ffc6a3717ad5
OmgCopito95 <july1995bolt@gmail.com>
2022-08-06 20:33:07 -0300

    constantes guardan info de la base de datos

--- src/const/globalConstants.js
+++ src/const/globalConstants.js
@@ -4,5 +4,7 @@ require('dotenv').config() // importar dotenv para obtener las variables de ento
 
 module.exports = {
     PORT: process.env.PORT || 5000, // obtener el puerto de la aplicación desde el archivo .env o si no existe, usar el puerto 5000
-
+    DB_NAME: process.env.DB_NAME,
+    DB_USERNAME: process.env.DB_USERNAME,
+    DB_PASSWORD: process.env.DB_PASSWORD
 }
